c5ed3d3ad427b3152319cc88e8abc427e816e7ab
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-06-25 16:19:47 -0400

    changed navbar to header only

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,9 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <div className="ui pointing menu">
-                <a className="item active" href="javascript:void(0)">
-                    Home
-            </a>
-                <a className="item" href="javascript:void(0)">
-                    About
-            </a>
-                <div className="right menu">
-                    <div className="item">
-                        <div className="ui transparent icon input">
-                            <input type="text" placeholder="Search..." />
-                            <i className="search link icon"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h3 className="ui center aligned  block header">
+                Pocket Space
+            </h3>
         </div>
     )
 }
