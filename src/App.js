a555e822ad39720520ff4016835bdf42b48f6164
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-06-25 16:18:10 -0400

    used hooks to maintain state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React,{useState , useEffect} from 'react';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 
 function App() {
+
+  const [isSelected, setIsSelected] = useState(true)
+  const [result , setResult] = useState([]);
+
+  useEffect(()=>{
+      let topics = [
+          { "id": 1, "title": "Space" },
+          { "id": 2, "title": "Big Bang Theory" },
+          { "id": 3, "title": "Galaxy"},
+          { "id": 4, "title": "Universe" },
+          { "id": 5, "title": "Solar System" }
+      ];
+      setResult(topics)
+  },[])
+
+  const handleClick = (e) => {
+    setIsSelected(false);
+}
+
   return (
     <div>
       <Header />
       <div className="ui raised segment">
-        <Dashboard />
+        <Dashboard result={result} handleClick={handleClick} isSelected={isSelected} />
       </div>
     </div>
   );
