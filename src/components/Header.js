2d7ddd8016ae29c8c10e4d9f40b732b5d155778f
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2020-11-13 23:52:32 -0800

    changed light to dark header color

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <h3 className="ui center aligned  block header">
+            <h3 className="ui center aligned inverted block header">
                 Pocket Space
             </h3>
         </div>
