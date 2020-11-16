4530bf67386fb5969dba1f00a6864576606491c6
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2020-11-15 22:19:55 -0800

    used style css from header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import '../styles/header.css'
 function Header() {
     return (
-        <div>
+        <div className="header-wrapper">
             <h3 className="ui center aligned inverted block header">
                 Pocket Space
             </h3>
