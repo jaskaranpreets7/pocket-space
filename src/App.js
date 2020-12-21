61b8013bd66b8280d1990d4261600464924a1b81
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2020-12-20 23:15:17 -0800

    refactor minor code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React,{useState , useEffect} from 'react';
 import Header from './components/Header';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard.tsx';
 
 function App() {
 
   const [isSelected, setIsSelected] = useState(true)
-  const [result , setResult] = useState([]);
+  const [subHeader , setSubHeader] = useState([]);
 
   useEffect(()=>{
       let topics = [
-          { "id": 1, "title": "Space", 'hasDetails' : false },
-          { "id": 2, "title": "Big Bang Theory" , 'hasDetails' : false},
-          { "id": 3, "title": "Galaxy"  ,'hasDetails' : false},
-          { "id": 4, "title": "Universe",  'hasDetails' : true },
-          { "id": 5, "title": "Solar System", 'hasDetails' : true }
+          { id: 1, "title": "Space", 'hasDetails' : false },
+          { id: 2, "title": "Big Bang Theory" , 'hasDetails' : false},
+          { id: 3, "title": "Galaxy"  ,'hasDetails' : false},
+          { id: 4, "title": "Universe",  'hasDetails' : true },
+          { id: 5, "title": "Solar System", 'hasDetails' : true }
       ];
-      setResult(topics)
+      setSubHeader(topics)
   },[])
 
-  const handleClick = (e) => {
+  const handleSubHeaderClick = (e) => {
     setIsSelected(false);
   }
 
   return (
     <div style={{ width:'100%', height:'100vh', overflow:'unset'}}>
       <Header />
-      <Dashboard result={result} handleClick={handleClick} isSelected={isSelected} />
+      <Dashboard subHeader={subHeader} handleSubHeaderClick={handleSubHeaderClick} isSelected={isSelected} />
     </div>
   );
 }
