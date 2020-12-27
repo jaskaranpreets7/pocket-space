bc6c61702ef0718a96d68ae73b4c1f37afe9eaa7
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2020-12-27 13:45:44 -0800

    added typescript extentsion broken file

---
+++ src/components/SolarSytem.tsx
@@ -0,0 +1,139 @@
+import React, { useState, Fragment } from 'react';
+import { Bounce} from 'react-reveal';
+import RubberBand from 'react-reveal/RubberBand';
+
+import solarImg from '../assets/yat.gif';
+import planetList from '../planets.json';
+import '../styles/solarsystem.css';
+
+export default function SolarSytem() {
+    const [check, setCheck] = useState(false);
+    const [compare1, setCompare1] = useState('');
+    const [secondValue, setSecondValue] = useState(false);
+    const [compare2, setCompare2] = useState('');
+    const onCheck = () => {
+        setCheck(true);
+    }
+
+    const renderList = () => {
+        return Object.keys(planetList.sections).map((planet) => {
+            return (
+                <Bounce top key={planet}>
+                    <div className="ui card" draggable="true" onDragStart={(e) => { onDragStart(e, planet) }} style={{ cursor: 'pointer' }}>
+                        <div className="content">{planet}</div>
+                    </div>
+                </Bounce>
+            )
+        })
+    }
+
+    const onDragStart = (e, id) => {
+        e.dataTransfer.setData('id', id)
+    }
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+    }
+
+    const onDrop1 = (e) => {
+        let id = e.dataTransfer.getData('id');
+        setCompare1(id)
+
+    }
+
+    const onDrop2 = (e) => {
+        let id = e.dataTransfer.getData('id');
+        setCompare2(id);
+        setSecondValue(true);
+    };
+
+    const showImage = () => {
+        setCheck(false)
+    };
+
+    const compare1Details = (compare1) => {
+        if (compare1.length > 0) {
+            return planetList.sections[compare1].map((item, index) => {
+                return (
+                    <RubberBand key={index}>
+                        <div>             
+                            <h4>Distance</h4><em>{item.Distance}</em>
+                            <h4>Mass</h4><em>{item.Mass}</em>
+                            <h4>Volume</h4><em>{item.Volume}</em>
+                            <h4>Temperature</h4><em>{item.Temperature}</em>
+                        </div>
+                    </RubberBand>
+                )
+            })
+        }
+    }
+
+
+    const compare2Details = (compare2) => {
+        if (compare2.length > 0) {
+            return planetList.sections[compare2].map((item, index) => {
+                return (
+                    <RubberBand key={index}>
+                        <div>
+                            <h4>Distance</h4><em>{item.Distance}</em>
+                            <h4>Mass</h4><em>{item.Mass}</em>
+                            <h4>Volume</h4><em>{item.Volume}</em>
+                            <h4>Temperature</h4><em>{item.Temperature}</em>   
+                        </div> 
+                    </RubberBand>
+                )
+            })
+        }
+    }
+
+    const onRefresh = () => {
+        setCompare1('');
+        setCompare2('');
+    }
+
+    return (
+        <div>
+            <div className="ui grid">
+                <div className="seven wide column">
+                    {renderList()}
+                </div>
+                <div className="nine wide column">
+                    <div className="btn-pos">
+                        {(check) ?
+                            <Fragment><button className="ui button" onClick={showImage}><i className="align justify icon"></i>Show Image</button> <button onClick={onRefresh} className="ui button">Refresh</button></Fragment>
+                            : <button className="ui button" onClick={onCheck}><i className="align justify icon"></i>Compare two planets</button>}
+                    </div>
+                    {(check) ?
+                        <Fragment>
+                            <div className="ui grid">
+                                <div className="eight wide column"
+                                    onDragOver={(e) => { onDragOver(e) }}
+                                    onDrop={(e) => { onDrop1(e) }}
+                                >
+                                    {(compare1.length) ? <div className="ui card" style={{ textAlign: 'center' }}><div className="content">{compare1}</div></div> : <span>Drag 1st planet and Drop here</span>}
+                                </div>
+                                <div className="eight wide column"
+                                    onDragOver={(e) => { onDragOver(e) }}
+                                    onDrop={(e) => { onDrop2(e) }}>
+                                    {(compare2.length) ? <div className="ui card" style={{ textAlign: 'center' }} ><div className="content">{compare2}</div></div> : <span>Drag 2nd planet and Drop here</span>}
+                                </div>
+                            </div>
+                            {(secondValue) ?
+                                <Fragment>
+                                    {(compare1 !== compare2) ?
+                                        <div className="ui grid">
+                                            <div className="eight wide column">
+                                                {compare1Details(compare1)}
+                                            </div>
+                                            <div className="eight wide column">
+                                                {compare2Details(compare2)}
+                                            </div>
+                                        </div> : <div className="compare-text">{(compare1.length === 0 && compare2.length === 0) ? null : <h3><em>Can't compare same terrestrial bodies</em></h3>}</div>}
+                                </Fragment> : null}
+                        </Fragment> : <img className="responsive" src={solarImg} alt="solar-system" />}
+                </div>
+            </div>
+        </div>
+
+    )
+}
